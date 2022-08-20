3966f128055a301331d042ac60693b30cf4562dc
Thanh Y <thanhy123zzz@gmail.com>
2022-08-21 00:56:19 +0700

    ad

--- src/main/java/com/filmnew/Dao/UserDao.java
+++ src/main/java/com/filmnew/Dao/UserDao.java
@@ -5,21 +5,21 @@ import java.util.List;
 import com.filmnew.Enity.User;
 
 public interface UserDao {
-	int add(User user);
-	
-	List<User> findOne(String name);
-
-	List<User> findlistbyname(String name);
-
-	List<User> pageofUser(int page);
-	
-	int delete(String name);
-	
-	int update(User user);
-	
-	List<User> findAll();
-
-	List<User> login(String name, String pass);
+//	int add(User user);
+//	
+//	List<User> findOne(String name);
+//
+//	List<User> findlistbyname(String name);
+//
+//	List<User> pageofUser(int page);
+//	
+//	int delete(String name);
+//	
+//	int update(User user);
+//	
+//	List<User> findAll();
+//
+//	List<User> login(String name, String pass);
 
 	//Boolean checkExistUser(String name);
 
--- src/main/java/com/filmnew/controller/ManagerController.java
+++ src/main/java/com/filmnew/controller/ManagerController.java
@@ -18,119 +18,119 @@ import com.filmnew.Service.UserService;
 
 @RestController
 public class ManagerController {
-
-	@Autowired
-	private UserService userService;
-
-	private ModelAndView mav = new ModelAndView();
-
-	// Login and SignUp
-
-	@PostMapping("/login")
-	public ModelAndView login(@ModelAttribute(name = "user") User user, HttpSession session) {
-		List<User> list = userService.login(user.getName(), user.getPass());
-		session.setAttribute("user", user);
-		session.setMaxInactiveInterval(60 * 60 * 24);
-		if (list.size() > 0) {
-			User user1 = userService.finpOne(user.getName());
-			if (user1.getIdfunction() == 1) {
-				mav.setViewName("manager-user");
-				return mav;
-			} else {
-				mav.setViewName("index");
-				return mav;
-			}
-		} else {
-			mav.addObject("messUser", "Username Or Pass Not Exist!");
-			mav.setViewName("index");
-			return mav;
-		}
-	}
-
-	// Manager
-
-	@GetMapping("/manager-user")
-	public ModelAndView loadInforUser() {
-		List<User> list = userService.findAll();
-		mav.addObject("listUser", list);
-		mav.addObject("num", 0);
-		mav.setViewName("manager-user");
-		mav.addObject("keyword", "");
-		return mav;
-	}
-
-	@PostMapping({ "/manager-user/addUser" })
-	public ModelAndView createUser(User user) {
-		try {
-			if (userService.finpOne(user.getName()).equals(null)) {
-				userService.add(user);
-			} else {
-				throw new DuplicateKeyException("Tài khoản đã tồn tại");
-
-			}
-		} catch (IndexOutOfBoundsException exception) {
-			userService.add(user);
-			mav.setViewName("manager-user");
-		}
-		return mav;
-	}
-
-	@PostMapping("/manager-user/editUser")
-	public ModelAndView updateUser(User user) {
-		userService.update(user);
-		return mav;
-	}
-
-	@GetMapping({ "/manager-user/findOne" })
-	public User findUser(String name) {
-		User user = userService.finpOne(name);
-		return user;
-	}
-
-	@GetMapping({ "/manager-user/search" })
-	public ModelAndView searchUser(@RequestParam("keyword") String search) {
-		if (search == null) {
-			List<User> list = userService.findAll();
-			mav.addObject("listUser", list);
-			mav.addObject("keyword", search);
-			return mav;
-
-		} else {
-			List<User> list = userService.findlistbyname(search);
-			mav.addObject("listUser", list);
-			mav.addObject("keyword", search);
-			return mav;
-		}
-	}
-
-	@GetMapping("/manager-user/removeUser")
-	public ModelAndView deleteUser(String name) {
-		userService.delete(name);
-		return mav;
-	}
-
-	@GetMapping("/manager-user/delete")
-	public ModelAndView delete(String name) {
-		userService.delete(name);
-		return mav;
-	}
-
-	@GetMapping("/manager-film")
-	public ModelAndView loadInforFilm() {
-		mav.setViewName("manager-film");
-		return mav;
-	}
-
-	@GetMapping("/manager-comment")
-	public ModelAndView loadInforComment() {
-		mav.setViewName("manager-comment");
-		return mav;
-	}
-
-	@GetMapping("/manager-favorite")
-	public ModelAndView loadInforFavorite() {
-		mav.setViewName("manager-favorite");
-		return mav;
-	}
+//
+//	@Autowired
+//	private UserService userService;
+//
+//	private ModelAndView mav = new ModelAndView();
+//
+//	// Login and SignUp
+//
+//	@PostMapping("/login")
+//	public ModelAndView login(@ModelAttribute(name = "user") User user, HttpSession session) {
+//		List<User> list = userService.login(user.getName(), user.getPass());
+//		session.setAttribute("user", user);
+//		session.setMaxInactiveInterval(60 * 60 * 24);
+//		if (list.size() > 0) {
+//			User user1 = userService.finpOne(user.getName());
+//			if (user1.getIdfunction() == 1) {
+//				mav.setViewName("manager-user");
+//				return mav;
+//			} else {
+//				mav.setViewName("index");
+//				return mav;
+//			}
+//		} else {
+//			mav.addObject("messUser", "Username Or Pass Not Exist!");
+//			mav.setViewName("index");
+//			return mav;
+//		}
+//	}
+//
+//	// Manager
+//
+//	@GetMapping("/manager-user")
+//	public ModelAndView loadInforUser() {
+//		List<User> list = userService.findAll();
+//		mav.addObject("listUser", list);
+//		mav.addObject("num", 0);
+//		mav.setViewName("manager-user");
+//		mav.addObject("keyword", "");
+//		return mav;
+//	}
+//
+//	@PostMapping({ "/manager-user/addUser" })
+//	public ModelAndView createUser(User user) {
+//		try {
+//			if (userService.finpOne(user.getName()).equals(null)) {
+//				userService.add(user);
+//			} else {
+//				throw new DuplicateKeyException("Tài khoản đã tồn tại");
+//
+//			}
+//		} catch (IndexOutOfBoundsException exception) {
+//			userService.add(user);
+//			mav.setViewName("manager-user");
+//		}
+//		return mav;
+//	}
+//
+//	@PostMapping("/manager-user/editUser")
+//	public ModelAndView updateUser(User user) {
+//		userService.update(user);
+//		return mav;
+//	}
+//
+//	@GetMapping({ "/manager-user/findOne" })
+//	public User findUser(String name) {
+//		User user = userService.finpOne(name);
+//		return user;
+//	}
+//
+//	@GetMapping({ "/manager-user/search" })
+//	public ModelAndView searchUser(@RequestParam("keyword") String search) {
+//		if (search == null) {
+//			List<User> list = userService.findAll();
+//			mav.addObject("listUser", list);
+//			mav.addObject("keyword", search);
+//			return mav;
+//
+//		} else {
+//			List<User> list = userService.findlistbyname(search);
+//			mav.addObject("listUser", list);
+//			mav.addObject("keyword", search);
+//			return mav;
+//		}
+//	}
+//
+//	@GetMapping("/manager-user/removeUser")
+//	public ModelAndView deleteUser(String name) {
+//		userService.delete(name);
+//		return mav;
+//	}
+//
+//	@GetMapping("/manager-user/delete")
+//	public ModelAndView delete(String name) {
+//		userService.delete(name);
+//		return mav;
+//	}
+//
+//	@GetMapping("/manager-film")
+//	public ModelAndView loadInforFilm() {
+//		mav.setViewName("manager-film");
+//		return mav;
+//	}
+//
+//	@GetMapping("/manager-comment")
+//	public ModelAndView loadInforComment() {
+//		mav.setViewName("manager-comment");
+//		return mav;
+//	}
+//
+//	@GetMapping("/manager-favorite")
+//	public ModelAndView loadInforFavorite() {
+//		mav.setViewName("manager-favorite");
+//		return mav;
+//	}
 
 }
