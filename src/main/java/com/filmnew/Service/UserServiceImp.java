df41370865f5f8287746324cbc871a5296fce6cc
Thanh Y <thanhy123zzz@gmail.com>
2022-08-21 00:38:07 +0700

    sua 1 so thu

--- src/main/java/com/filmnew/Service/UserServiceImp.java
+++ src/main/java/com/filmnew/Service/UserServiceImp.java
@@ -10,53 +10,53 @@ import com.filmnew.Enity.User;
 
 @Service
 public class UserServiceImp implements UserService{
-//
-//	@Autowired
-//	private UserDao userDao;
-//	
-//	@Override
-//	public int delete(String name) {
-//		return userDao.delete(name);
-//	}
-//
-//	@Override
-//	public int add(User user) {
-//		return userDao.add(user);
-//	}
-//
-//	@Override
-//	public User finpOne(String name) {
-//		return userDao.findOne(name).get(0);
-//	}
-//
-//	@Override
-//	public int update(User user) {
-//		return userDao.update(user);
-//	}
-//
-//	@Override
-//	public List<User> findAll() {
-//		return userDao.findAll();
-//	}
-//
-//	@Override
-//	public List<User> findlistbyname(String name){
-//		return userDao.findlistbyname(name);
-//	}
-//
-//	@Override
-//	public List<User> login(String name, String pass){
-//		return userDao.login(name,pass);
-//	}
-//
-//	/*@Override
-//	public Boolean checkExistUser(String name){
-//		return userDao.checkExistUser(name);
-//	}
-// */
-//	@Override
-//	public List<User> pageofUser(int page){
-//		return userDao.pageofUser(page);
-//	}
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Override
+	public int delete(String name) {
+		return userDao.delete(name);
+	}
+
+	@Override
+	public int add(User user) {
+		return userDao.add(user);
+	}
+
+	@Override
+	public User finpOne(String name) {
+		return userDao.findOne(name).get(0);
+	}
+
+	@Override
+	public int update(User user) {
+		return userDao.update(user);
+	}
+
+	@Override
+	public List<User> findAll() {
+		return userDao.findAll();
+	}
+
+	@Override
+	public List<User> findlistbyname(String name){
+		return userDao.findlistbyname(name);
+	}
+
+	@Override
+	public List<User> login(String name, String pass){
+		return userDao.login(name,pass);
+	}
+
+	/*@Override
+	public Boolean checkExistUser(String name){
+		return userDao.checkExistUser(name);
+	}
+ */
+	@Override
+	public List<User> pageofUser(int page){
+		return userDao.pageofUser(page);
+	}
 
 }
